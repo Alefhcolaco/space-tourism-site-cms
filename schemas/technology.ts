1c7f9fdda7106abb5f8e400e639dd3fc65314b39
Alefh Colaço Charquero <92604413+Alefhyy@users.noreply.github.com>
2023-09-27 15:07:46 -0300

    feat: cms pagina technology

--- schemas/technology.ts
+++ schemas/technology.ts
@@ -8,5 +8,60 @@ export default {
             name: 'title',
             type: 'string'
         },
+        {
+            title: 'Span number',
+            name: 'spanNumber',
+            type: 'number'
+        },
+        {
+            title: 'Title hero',
+            name: 'titleHero',
+            type: 'string'
+        },
+        {
+            title: 'Technology cards',
+            name: 'technologyCards',
+            type: 'array',
+            of: [
+                {
+                    title: 'Cards',
+                    name: 'cards',
+                    type: 'object',
+                    fields: [
+                        {
+                            title: 'Image',
+                            name: 'image',
+                            type: 'image',
+                            options: {
+                                hotspot: true
+                            },
+                            fields: [
+                                {
+                                    title: 'Alt',
+                                    name: 'alt',
+                                    type: 'string',
+                                }
+                            ]
+                        },
+                        {
+                            title: 'Card title',
+                            name: 'cardTitle',
+                            type: 'string'
+                        },
+                        {
+                            title: 'Card subtitle',
+                            name: 'cardSubtitle',
+                            type: 'string'
+                        },
+                        {
+                            title: 'Card text',
+                            name: 'cardText',
+                            type: 'text'
+                        },
+                        
+                    ]
+                }
+            ]
+        }
     ],
 }
