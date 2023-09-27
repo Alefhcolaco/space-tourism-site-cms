c7a304d7c8981ba9fc161ba0eb77d28734ac9905
Alefh Colaço Charquero <92604413+Alefhyy@users.noreply.github.com>
2023-09-27 10:36:05 -0300

    feat: cms pagina destination

--- schemas/destination.ts
+++ schemas/destination.ts
@@ -8,5 +8,74 @@ export default {
             name: 'title',
             type: 'string'
         },
+        {
+            title: 'Span number',
+            name: 'spanNumber',
+            type: 'number'
+        },
+        {
+            title: 'Title hero',
+            name: 'titleHero',
+            type: 'string'
+        },
+        {
+            title: 'Planets cards',
+            name: 'planetsCards',
+            type: 'array',
+            of: [
+                {
+                    title: 'Cards',
+                    name: 'cards',
+                    type: 'object',
+                    fields: [
+                        {
+                            title: 'Planet',
+                            name: 'planet',
+                            type: 'image',
+                            options: {
+                                hotspot: true
+                            },
+                            fields: [
+                                {
+                                    title: 'Alt',
+                                    name: 'alt',
+                                    type: 'string',
+                                }
+                            ]
+                        },
+                        {
+                            title: 'Card title',
+                            name: 'cardTitle',
+                            type: 'string'
+                        },
+                        {
+                            title: 'Card text',
+                            name: 'cardText',
+                            type: 'text'
+                        },
+                        {
+                            title: 'Average distance title',
+                            name: 'averageDistanceTitle',
+                            type: 'text'
+                        },
+                        {
+                            title: 'Average distance number',
+                            name: 'averageDistanceNumber',
+                            type: 'text'
+                        },
+                        {
+                            title: 'Estimated time title',
+                            name: 'estimatedTimeTitle',
+                            type: 'text'
+                        },
+                        {
+                            title: 'Estimated time number',
+                            name: 'estimatedTimeNumber',
+                            type: 'text'
+                        }
+                    ]
+                }
+            ]
+        }
     ],
 }
