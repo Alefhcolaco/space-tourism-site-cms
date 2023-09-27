d3bacab6f4b9fe1a94507f5f038f37176918133e
Alefh Colaço Charquero <92604413+Alefhyy@users.noreply.github.com>
2023-09-27 14:41:12 -0300

    feat: cms pagina crew

--- schemas/crew.ts
+++ schemas/crew.ts
@@ -8,5 +8,59 @@ export default {
             name: 'title',
             type: 'string'
         },
+        {
+            title: 'Span number',
+            name: 'spanNumber',
+            type: 'number'
+        },
+        {
+            title: 'Title hero',
+            name: 'titleHero',
+            type: 'string'
+        },
+        {
+            title: 'Crew cards',
+            name: 'crewCards',
+            type: 'array',
+            of: [
+                {
+                    title: 'Cards',
+                    name: 'cards',
+                    type: 'object',
+                    fields: [
+                        {
+                            title: 'Crew member',
+                            name: 'crewMember',
+                            type: 'image',
+                            options: {
+                                hotspot: true
+                            },
+                            fields: [
+                                {
+                                    title: 'Alt',
+                                    name: 'alt',
+                                    type: 'string',
+                                }
+                            ]
+                        },
+                        {
+                            title: 'Member name',
+                            name: 'memberName',
+                            type: 'string'
+                        },
+                        {
+                            title: 'Office',
+                            name: 'office',
+                            type: 'string'
+                        },
+                        {
+                            title: 'Text',
+                            name: 'text',
+                            type: 'text'
+                        },
+                    ]
+                }
+            ]
+        }
     ],
 }
