fbbf79e98ac85db76f95fed1b89001dce0a0ee49
Alefh Colaço Charquero <92604413+Alefhyy@users.noreply.github.com>
2023-10-02 11:27:19 -0300

    teste

--- schemas/geral.ts
+++ schemas/geral.ts
@@ -1,43 +1,43 @@
-export default {
-    title: 'Geral',
-    name: 'geral',
-    type: 'document',
-    fields: [
-        {
-            title: 'Title',
-            name: 'title',
-            type: 'string'
-        },
-        {
-            name: 'navigation',
-            type: 'array',
-            title: 'Navegação',
-            of: [
-                {
-                    name: 'navigationItem',
-                    type: 'object',
-                    title: 'Item de navegação',
-                    fields: [
-                        {
-                            title: 'Number',
-                            name: 'number',
-                            type: 'string',
-                        },
-                        {
-                            name: 'title',
-                            type: 'string',
-                            title: 'Título',
-                            validation: (Rule: any) => Rule.required(),
-                        },
-                        {
-                            name: 'link',
-                            type: 'string',
-                            title: 'Link',
-                            validation: (Rule: any) => Rule.required(),
-                        },
-                    ],
-                },
-            ],
-        },
-    ],
-}
+// export default {
+//     title: 'Geral',
+//     name: 'geral',
+//     type: 'document',
+//     fields: [
+//         {
+//             title: 'Title',
+//             name: 'title',
+//             type: 'string'
+//         },
+//         {
+//             name: 'navigation',
+//             type: 'array',
+//             title: 'Navegação',
+//             of: [
+//                 {
+//                     name: 'navigationItem',
+//                     type: 'object',
+//                     title: 'Item de navegação',
+//                     fields: [
+//                         {
+//                             title: 'Number',
+//                             name: 'number',
+//                             type: 'string',
+//                         },
+//                         {
+//                             name: 'title',
+//                             type: 'string',
+//                             title: 'Título',
+//                             validation: (Rule: any) => Rule.required(),
+//                         },
+//                         {
+//                             name: 'link',
+//                             type: 'string',
+//                             title: 'Link',
+//                             validation: (Rule: any) => Rule.required(),
+//                         },
+//                     ],
+//                 },
+//             ],
+//         },
+//     ],
+// }
