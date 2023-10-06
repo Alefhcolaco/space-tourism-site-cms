b02379bc0559bd764ee8f9f7ec9ca258ee9dcfd8
Alefh Colaço Charquero <92604413+Alefhyy@users.noreply.github.com>
2023-10-06 15:40:20 -0300

    fix: planets cards

--- schemas/destination.ts
+++ schemas/destination.ts
@@ -19,82 +19,280 @@ export default {
             type: 'string'
         },
         {
-            title: 'Navigation tabs',
-            name: 'navigationTabs',
-            type: 'array',
-            of: [
+            title: 'Moon card',
+            name: 'moonCard',
+            type: 'object',
+            fields: [
                 {
-                    title: 'Tab links',
-                    name: 'tabLinks',
-                    type: 'object',
+                    title: 'Planet',
+                    name: 'planet',
+                    type: 'image',
+                    options: {
+                        hotspot: true
+                    },
                     fields: [
                         {
-                            title: 'Tab link',
-                            name: 'link',
+                            title: 'Alt',
+                            name: 'alt',
                             type: 'string',
                         }
                     ]
-                }
-            ],
-        },
-        {
-            title: 'Planets cards',
-            name: 'planetsCards',
-            type: 'array',
-            of: [
+                },
                 {
-                    title: 'Cards',
-                    name: 'cards',
-                    type: 'object',
-                    fields: [
-                        {
-                            title: 'Planet',
-                            name: 'planet',
-                            type: 'image',
-                            options: {
-                                hotspot: true
-                            },
-                            fields: [
-                                {
-                                    title: 'Alt',
-                                    name: 'alt',
-                                    type: 'string',
-                                }
-                            ]
-                        },
-                        {
-                            title: 'Card title',
-                            name: 'cardTitle',
-                            type: 'string'
-                        },
-                        {
-                            title: 'Card text',
-                            name: 'cardText',
-                            type: 'text'
-                        },
-                        {
-                            title: 'Average distance title',
-                            name: 'averageDistanceTitle',
-                            type: 'text'
-                        },
-                        {
-                            title: 'Average distance number',
-                            name: 'averageDistanceNumber',
-                            type: 'text'
-                        },
-                        {
-                            title: 'Estimated time title',
-                            name: 'estimatedTimeTitle',
-                            type: 'text'
-                        },
-                        {
-                            title: 'Estimated time number',
-                            name: 'estimatedTimeNumber',
-                            type: 'text'
-                        }
-                    ]
+                    title: 'Card title',
+                    name: 'cardTitle',
+                    type: 'string'
+                },
+                {
+                    title: 'Card text',
+                    name: 'cardText',
+                    type: 'text'
+                },
+                {
+                    title: 'Average distance title',
+                    name: 'averageDistanceTitle',
+                    type: 'text'
+                },
+                {
+                    title: 'Average distance number',
+                    name: 'averageDistanceNumber',
+                    type: 'text'
+                },
+                {
+                    title: 'Estimated time title',
+                    name: 'estimatedTimeTitle',
+                    type: 'text'
+                },
+                {
+                    title: 'Estimated time number',
+                    name: 'estimatedTimeNumber',
+                    type: 'text'
                 }
             ]
-        }
+        },
+        {
+            title: 'Mars card',
+            name: 'marsCard',
+            type: 'object',
+            fields: [
+                {
+                    title: 'Planet',
+                    name: 'planet',
+                    type: 'image',
+                    options: {
+                        hotspot: true
+                    },
+                    fields: [
+                        {
+                            title: 'Alt',
+                            name: 'alt',
+                            type: 'string',
+                        }
+                    ]
+                },
+                {
+                    title: 'Card title',
+                    name: 'cardTitle',
+                    type: 'string'
+                },
+                {
+                    title: 'Card text',
+                    name: 'cardText',
+                    type: 'text'
+                },
+                {
+                    title: 'Average distance title',
+                    name: 'averageDistanceTitle',
+                    type: 'text'
+                },
+                {
+                    title: 'Average distance number',
+                    name: 'averageDistanceNumber',
+                    type: 'text'
+                },
+                {
+                    title: 'Estimated time title',
+                    name: 'estimatedTimeTitle',
+                    type: 'text'
+                },
+                {
+                    title: 'Estimated time number',
+                    name: 'estimatedTimeNumber',
+                    type: 'text'
+                }
+            ]
+        },
+        {
+            title: 'Europa card',
+            name: 'europaCard',
+            type: 'object',
+            fields: [
+                {
+                    title: 'Planet',
+                    name: 'planet',
+                    type: 'image',
+                    options: {
+                        hotspot: true
+                    },
+                    fields: [
+                        {
+                            title: 'Alt',
+                            name: 'alt',
+                            type: 'string',
+                        }
+                    ]
+                },
+                {
+                    title: 'Card title',
+                    name: 'cardTitle',
+                    type: 'string'
+                },
+                {
+                    title: 'Card text',
+                    name: 'cardText',
+                    type: 'text'
+                },
+                {
+                    title: 'Average distance title',
+                    name: 'averageDistanceTitle',
+                    type: 'text'
+                },
+                {
+                    title: 'Average distance number',
+                    name: 'averageDistanceNumber',
+                    type: 'text'
+                },
+                {
+                    title: 'Estimated time title',
+                    name: 'estimatedTimeTitle',
+                    type: 'text'
+                },
+                {
+                    title: 'Estimated time number',
+                    name: 'estimatedTimeNumber',
+                    type: 'text'
+                }
+            ]
+        },
+        {
+            title: 'Titan card',
+            name: 'titanCard',
+            type: 'object',
+            fields: [
+                {
+                    title: 'Planet',
+                    name: 'planet',
+                    type: 'image',
+                    options: {
+                        hotspot: true
+                    },
+                    fields: [
+                        {
+                            title: 'Alt',
+                            name: 'alt',
+                            type: 'string',
+                        }
+                    ]
+                },
+                {
+                    title: 'Card title',
+                    name: 'cardTitle',
+                    type: 'string'
+                },
+                {
+                    title: 'Card text',
+                    name: 'cardText',
+                    type: 'text'
+                },
+                {
+                    title: 'Average distance title',
+                    name: 'averageDistanceTitle',
+                    type: 'text'
+                },
+                {
+                    title: 'Average distance number',
+                    name: 'averageDistanceNumber',
+                    type: 'text'
+                },
+                {
+                    title: 'Estimated time title',
+                    name: 'estimatedTimeTitle',
+                    type: 'text'
+                },
+                {
+                    title: 'Estimated time number',
+                    name: 'estimatedTimeNumber',
+                    type: 'text'
+                }
+            ]
+        },
     ],
 }
+
+
+
+
+
+
+
+
+
+// {
+//     title: 'Planets cards',
+//     name: 'planetsCards',
+//     type: 'array',
+//     of: [
+        // {
+        //     title: 'Cards',
+        //     name: 'cards',
+        //     type: 'object',
+        //     fields: [
+        //         {
+        //             title: 'Planet',
+        //             name: 'planet',
+        //             type: 'image',
+        //             options: {
+        //                 hotspot: true
+        //             },
+        //             fields: [
+        //                 {
+        //                     title: 'Alt',
+        //                     name: 'alt',
+        //                     type: 'string',
+        //                 }
+        //             ]
+        //         },
+        //         {
+        //             title: 'Card title',
+        //             name: 'cardTitle',
+        //             type: 'string'
+        //         },
+        //         {
+        //             title: 'Card text',
+        //             name: 'cardText',
+        //             type: 'text'
+        //         },
+        //         {
+        //             title: 'Average distance title',
+        //             name: 'averageDistanceTitle',
+        //             type: 'text'
+        //         },
+        //         {
+        //             title: 'Average distance number',
+        //             name: 'averageDistanceNumber',
+        //             type: 'text'
+        //         },
+        //         {
+        //             title: 'Estimated time title',
+        //             name: 'estimatedTimeTitle',
+        //             type: 'text'
+        //         },
+        //         {
+        //             title: 'Estimated time number',
+        //             name: 'estimatedTimeNumber',
+        //             type: 'text'
+        //         }
+        //     ]
+        // }
+//     ]
+// }
